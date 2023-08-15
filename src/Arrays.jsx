19ab862c5bede6769fcb45b8e17c6d62c26ba994
Bianca Santos <luisabianca93@gmail.com>
2023-08-15 02:55:10 -0300

    keys

--- src/Arrays.jsx
+++ src/Arrays.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
 const Arrays = () => {
+
+  const myFavorites = [
+    {nome: "Desperate Housewives", ano: 2004}, 
+    {nome: "Grey's Anatomy", ano: 2005},
+    {nome: "Mulheres de Areia", ano: 1993}, 
+    {nome: "Tieta", ano: 1989}, 
+    {nome: "Anjo Mau", ano: 1997}
+  ]
+
   return (
     <>
-      teste
+      <ul>
+        {myFavorites.map(({ nome, ano }) => (
+          <li key={nome}>
+            {nome} , {ano}
+          </li>
+        ))}
+      </ul>
     </>
   )
 
