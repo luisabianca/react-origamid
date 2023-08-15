d87703c0dd80a8fedbfb2bca2f80a0ec67ce6e12
Bianca Santos <luisabianca93@gmail.com>
2023-08-15 03:24:24 -0300

    finish arrays

--- src/Colors.jsx
+++ src/Colors.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
 
+const produtos = [
+  {
+    id: 1,
+    nome: 'Smartphone',
+    preco: 'R$ 2000',
+    cores: ['#29d8d5', '#252a34', '#fc3766'],
+  },
+  {
+    id: 2,
+    nome: 'Notebook',
+    preco: 'R$ 3000',
+    cores: ['#ffd045', '#d4394b', '#f37c59'],
+  },
+  {
+    id: 3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cores: ['#365069', '#47c1c8', '#f95786'],
+  },
+];
+
 const Colors = () => {
+  const dados = produtos.filter(({ preco }) => Number(preco.replace("R$ ", "")) > 1500)
+  console.log(dados)
 
   return (
-    <>
-      waiting...
-    </>
+    <section>
+      {dados.map(({ id, nome, preco, cores }) => (
+        <div key={id}>
+          <h1>{nome}</h1>
+          <p>Preço: {preco}</p>
+          <p>cores:</p>
+          <ul>{cores.map((item) => 
+            (<li key={item} style={{backgroundColor: item, color: "#fff"}}>{item}</li>)
+          )}</ul>
+        </div>
+      ))}
+
+    </section>
   )
 
 }
