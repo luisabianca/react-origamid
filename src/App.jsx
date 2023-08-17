4512344bb2d68029d4832ade8b09021d18307422
Bianca Santos <luisabianca93@gmail.com>
2023-08-17 01:09:05 -0300

    destruturando e adicionando mais itens

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Modal from './Modal';
 
 const App = () => {
   const [ modal, setModal ] = useState(false);
+  const [ contar, setContar ] = useState(1);
+  const [ items, setItems ] = useState(['Item 1'])
   const { pathname } = window.location;
 
   let Component;
@@ -16,15 +18,27 @@ const App = () => {
     Component = Home;
   }
 
+  const handleClick = () => {
+    setContar(contar + 1);
+    setItems(() => [...items, 'Item ' + (contar + 1)])
+  }
+
   return (
     <section>
       <Header />
       <div>
-        <div>{modal ? "Modal aberto" : "Modal fechado"}</div>
-        <Modal modal={modal} setModal={setModal}/>
+        {modal && 
+          <Modal modal={modal} setModal={setModal}/>
+        }
         <ButtonModal setModal={setModal} />
       </div>
       <Component />
+      <div>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+        <button onClick={handleClick}>{contar}</button>
+      </div>
     </section>
   );
 };
