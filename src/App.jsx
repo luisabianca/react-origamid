bf67c5aff84ab82f47830676696e9637a6fcf312
Bianca Santos <luisabianca93@gmail.com>
2023-08-19 02:53:27 -0300

    new organization for componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import Produto from './Produto';
-import { GlobalStorage } from './GlobalContext';
-import Limpar from './Limpar';
+import AppUseState from './UseState/AppUseState'
+import AppUseEffect from './UseEffect/AppUseEffect'
+import AppUseRef from './UseRef/AppUseRef'
+import AppUseContext  from './UseContext/AppUseContext';
+import AppUseMemoAndUseCallback from './UseMemoAndUseCallback/AppUseMemoAndUseCallback'
+import AppCustomHooks from './CustomHooks/AppCustomHooks'
 
 const App = () => {
   return (
-    <GlobalStorage>
-      <Produto />
-      <Limpar />
-    </GlobalStorage>
+    <>
+      {/* <AppUseState /> */}
+      {/* <AppUseEffect /> */}
+      {/* <AppUseRef /> */}
+      {/* <AppUseContext /> */}
+      {/* <AppUseMemoAndUseCallback /> */}
+      {/* <AppCustomHooks /> */}
+    
+    </>
   );
 };
 
